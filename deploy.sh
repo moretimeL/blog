48ce443d79e46ad3b97f0d11301f9e2bb0f8c764
jun <1057720671@qq.com>
2019-03-22 19:43:58 +0800

    更新服务器脚本

--- deploy.sh
+++ deploy.sh
@@ -3,7 +3,7 @@
 git pull
 mvn package -Dmaven.test.skip=true
 mkdir -p /data/jar
-cp target/blog.jar /data/jar/blog.jar
+cp target/blog-0.0.1-SNAPSHOT.jar /data/jar/blog.jar
 chmod +x /data/jar/blog.jar
 ln -s /data/jar/blog.jar /etc/init.d/blogservice
 service blogservice restart
