21ca4de3584252630ad156b14fccd10df128c17d
liu <1057720671@qq.com>
2019-03-25 20:00:33 +0800

    提交获取ip的工具类

---
+++ src/main/java/com/liu/utile/IpUtile.java
@@ -0,0 +1,31 @@
+package com.liu.utile;
+
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * @program: blog
+ * @author: smile liu
+ * @description:
+ * @create: 2019-03-25 19:57
+ **/
+
+public class IpUtile {
+    public static String getIp2(HttpServletRequest request) {
+        String ip = request.getHeader("X-Forwarded-For");
+        if(ip!=null && !"unKnown".equalsIgnoreCase(ip)){
+            //多次反向代理后会有多个ip值，第一个ip才是真实ip
+            int index = ip.indexOf(",");
+            if(index != -1){
+                return ip.substring(0,index);
+            }else{
+                return ip;
+            }
+        }
+        ip = request.getHeader("X-Real-IP");
+        if(ip!=null && !"unKnown".equalsIgnoreCase(ip)){
+            return ip;
+        }
+        return request.getRemoteAddr();
+    }
+
+}
